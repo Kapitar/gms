6465bbb8d912c96c4d4a05b8a1ec4142e06e206a
kapitar <kkapitar@ya.ru>
2024-05-20 20:51:26 +0900

    edited the frontend structure

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+const router = createRouter({
+    history: createWebHistory(),
+    routes: []
+})
 
+app.use(router)
 app.mount('#app')
